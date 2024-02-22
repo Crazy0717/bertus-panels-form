9d5854c6f289832e30f7f449e1bc2fc1ec87a01c
Crazy0717 <bpcrazy28@gamil.com>
2024-02-22 19:01:59 +0500

    v1.0

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
-export {default as Main} from "../pages/main"
-export {default as Users} from "../pages/users"
-export {default as PageNotFound} from "../pages/pageNotFound"
-export {default as MainForm} from "../components/mainForm"
-export {default as Loader} from "../components/Loader"
+export { default as Main } from "../pages/main";
+export { default as Users } from "../pages/users";
+export { default as PageNotFound } from "../pages/pageNotFound";
+export { default as MainForm } from "../components/mainForm";
+export { default as Loader } from "../components/Loader";
