db820de69ae021708c127eedc0dbcf8fc750bc32
Crazy0717 <bpcrazy28@gamil.com>
2024-02-23 17:16:49 +0500

    v1.92

--- src/ui/textArea.jsx
+++ src/ui/textArea.jsx
@@ -1,10 +1,10 @@
-const TextArea = ({ label, height, helpId }) => {
+const TextArea = ({ label, height, helpId = "floatingTextarea2" }) => {
   return (
     <div className="form-floating m-3">
       <textarea
         className="form-control textArea"
         placeholder={label}
-        style={{ height: height, paddingTop: "40px" }}
+        style={{ height: height, paddingTop: "40px", overflowWrap: "break-word" }}
         id={helpId}
       ></textarea>
       <label className="label-size" htmlFor={helpId}>
